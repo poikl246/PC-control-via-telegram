4b5df752a8356201f1d62589ba637b811de57f92
poikl246 <poikl.m246@gmail.com>
2021-10-29 20:10:00 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TOKEN = 'ВАШ ТОКЕН'
-KEY = 'IshfChgCeBgWVDLO2holg3DPGhZnohu8FGo7_cUXfIE='
+TOKEN = 'your telegram token'
+KEY = 'IshfChgCeBgWVDLO2holg3DPGhZnohu8FGo7_cUXfIE=' # the file encryption key, preferably replaced
